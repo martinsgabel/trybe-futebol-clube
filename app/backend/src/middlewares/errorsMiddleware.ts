0668621ff588fdf149ddccdf81e2f9328a37de72
martins <gabrielamspinola@gmail.com>
2022-11-13 22:38:29 -0300

    correção da rota de erros

--- app/backend/src/middlewares/errorsMiddleware.ts
+++ app/backend/src/middlewares/errorsMiddleware.ts
@@ -1,8 +1,8 @@
 import { ErrorRequestHandler } from 'express';
 
-const errorMiddleware: ErrorRequestHandler = (err, req, res, _next) => {
+const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
   if (err.status) {
-    return res.status(err.status).json({ error: err.message });
+    return res.status(err.status).json({ message: err.message });
   }
 
   return res.status(500).json({ error: err.message });
