3033d7217d9deba8b00006470c829db4488c32be
martins <gabrielamspinola@gmail.com>
2022-11-13 23:14:24 -0300

    chore: teste para a rota /matches

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,53 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import Matches from '../database/models/MatchesModel';
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teste da rota /matches', () => {
+  it('a rota /matches deve retornar todas as partidas', async() => {
+    const mock = [
+      {
+        "id": 1,
+        "homeTeam": 16,
+        "homeTeamGoals": 1,
+        "awayTeam": 8,
+        "awayTeamGoals": 1,
+        "inProgress": false,
+        "teamHome": {
+          "teamName": "São Paulo"
+        },
+        "teamAway": {
+          "teamName": "Grêmio"
+        }
+      },
+      {
+        "id": 41,
+        "homeTeam": 16,
+        "homeTeamGoals": 2,
+        "awayTeam": 9,
+        "awayTeamGoals": 0,
+        "inProgress": true,
+        "teamHome": {
+          "teamName": "São Paulo"
+        },
+        "teamAway": {
+          "teamName": "Internacional"
+        }
+      }
+    ]
+
+    sinon.stub(Matches, 'findAll').resolves(mock as unknown as Matches[]);
+
+    const httpResponse = await chai.request(app).get('/matches')
+      expect(httpResponse.status).to.equal(200)
+      expect(httpResponse.body).to.deep.equal(mock)
+    
+    sinon.restore();
+  })
+})
