f51a8e08e48ef57b18382d913a053c1745fa08e0
martins <gabrielamspinola@gmail.com>
2022-11-13 22:46:27 -0300

    correção na rota /matches

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,8 +1,14 @@
+import Teams from '../database/models/TeamsModel';
 import Matches from '../database/models/MatchesModel';
 
 export default class MatchesService {
   public getAllMatches = async () => {
-    const allMatches = await Matches.findAll();
+    const allMatches = await Matches.findAll({
+      include: [
+        { model: Teams, as: 'teamHome', attributes: ['teamName'] },
+        { model: Teams, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
     return allMatches;
   };
 }
