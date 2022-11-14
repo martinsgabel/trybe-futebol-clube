0d3009cdf624c433f24ae69435ce80a7f650577d
martins <gabrielamspinola@gmail.com>
2022-11-14 00:06:23 -0300

    chore: validação dos dados da rota /matches post

--- app/backend/src/errors/MatchError.ts
+++ app/backend/src/errors/MatchError.ts
@@ -1,7 +1,7 @@
-export default class MissingId extends Error {
+export default class MatchError extends Error {
   public status: number;
   constructor(message: string) {
     super(message);
-    this.status = 404;
+    this.status = 422;
   }
 }
--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,7 +1,9 @@
 import Teams from '../database/models/TeamsModel';
 import Matches from '../database/models/MatchesModel';
-import JWT from '../helpers/jwt';
+// import JWT from '../helpers/jwt';
 import UnauthorizedError from '../errors/UnauthorizedError';
+import MatchError from '../errors/MatchError';
+import MissingId from '../errors/MissingId';
 
 interface newMatch {
   homeTeam: number,
@@ -35,12 +37,24 @@ export default class MatchesService {
   };
 
   public saveMatch = async (match: newMatch, authorization: string) => {
-    const authorized = JWT.decodePassword(authorization);
+    if (!authorization) throw new UnauthorizedError('Token must be a valid token');
 
-    console.log(authorized);
+    if (match.awayTeam === match.homeTeam) {
+      throw new MatchError('It is not possible to create a match with two equal teams');
+    }
 
-    if (!authorized) throw new UnauthorizedError('Token must be valid');
+    // const authorized = JWT.decodePassword(authorization);
 
+    // if (!authorized) throw new UnauthorizedError('Token must be a valid token');
+
+    // checar se o id existe
+    const idAwayTeam = await Matches.findOne({ where: { id: match.awayTeam } });
+
+    const idHomeTeam = await Matches.findOne({ where: { id: match.homeTeam } });
+
+    if (!idAwayTeam || !idHomeTeam) throw new MissingId('There is no team with such id!');
+
+    // criando match
     const newMatch = await Matches.create(match);
 
     return newMatch;
